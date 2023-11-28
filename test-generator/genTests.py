54240d09840b22b5137ace4ffbb782eccab3ff52
xiaoyez <xiaoyez@zju.edu.cn>
2023-11-28 17:01:19 +0800

    compile system level kernel module template arm64

--- test-generator/genTests.py
+++ test-generator/genTests.py
@@ -1,10 +1,11 @@
 import os
 import pickle
 import argparse
-import click
+from math import ceil
 from tqdm import tqdm
 
 from utils.elf import parse_template
+from utils.utils import bin2bytes
 
 
 def generate(binary: bytes, offset: int, insts: dict, outdir: str):
@@ -14,31 +15,24 @@ def generate(binary: bytes, offset: int, insts: dict, outdir: str):
         with open(os.open(outpath, os.O_CREAT | os.O_WRONLY, 0o777), "wb") as f:
             f.write(binary)
 
-def kgenerate(binary: bytes, offset: int, offset_mem: int, instsfile, outdir: str, mode: str, testnum: int, dump: bool):
+def kgenerate(binary: bytes, offset: int, offset_mem: int, insts: dict, outdir: str, testnum: int, dump: bool):
     test_insts = []
-    dump_insts = []
     ARM_INST_LEN = 4
-    if mode=='arm':
-        NUM_INST = 17 
-    elif mode=='arm64':
-        NUM_INST = 0
-    insts_num = len(instsfile.readlines())
-    instsfile.seek(0)
+    insts_num = len(insts)
     print(insts_num)
-    for i, inst in enumerate(instsfile):
+    for i, (name, inst) in tqdm(insts.items()):
         if (i!=0 and i%testnum ==0) or i==insts_num-1:
             outbinary = binary[:offset] 
             temp_offset = offset
             for test_inst in test_insts:
-                outbinary += binary[temp_offset : temp_offset+ARM_INST_LEN*NUM_INST] +test_inst + binary[temp_offset+ARM_INST_LEN*(NUM_INST+1) : temp_offset + ARM_INST_LEN*(NUM_INST+2)]
-                temp_offset = temp_offset + ARM_INST_LEN*(NUM_INST+2)
+                outbinary += binary[temp_offset : temp_offset] +test_inst + binary[temp_offset+ARM_INST_LEN : temp_offset + ARM_INST_LEN*2]
+                temp_offset = temp_offset + ARM_INST_LEN*2
             if dump:
                 outbinary += binary[temp_offset : offset_mem] 
                 temp_offset = offset_mem
-                for dump_inst in dump_insts:
+                for dump_inst in test_insts:
                     outbinary += dump_inst + binary[temp_offset + len(dump_inst) : temp_offset + len(dump_inst)*2 ]
                     temp_offset = temp_offset + len(dump_inst)*2
-
                 outbinary += binary[temp_offset:]
             else:
                 outbinary += binary[temp_offset : ] 
@@ -50,16 +44,7 @@ def kgenerate(binary: bytes, offset: int, offset_mem: int, instsfile, outdir: st
                 f.write(outbinary)
             os.system('strip --strip-debug '+outpath)
             test_insts = []
-            dump_insts = []
-        if mode=='arm64':
-            inst = inst.split()[2].encode("ascii")
-        else:
-            inst = inst.split()[1].encode("ascii")
-        inst = bin2bytes(inst, mode)
-        sg = StoreTestCaseGenerator(mode)
-        dump_mem = sg.get_mem_inst(inst)
         test_insts.append(inst)
-        dump_insts.append(dump_mem)
 
 
 def parse_args():
@@ -68,33 +53,37 @@ def parse_args():
                         help='instruction file to be processed')
     parser.add_argument('--mode', choices=["arm", "thumb", "arm64"], required=True,
                         help='instruction mode')
-    parser.add_argument('--level', choices=["system", "user"], required=True,
-                        help='instruction mode')
     parser.add_argument('--outdir', type=str, default="testcases",
                         help='The name of the output kernel module')
+    parser.add_argument('--level', choices=["system", "user"], required=True,
+                        help='instruction mode')
+    parser.add_argument('--number', type=int, default=100000,
+                        help='The number of testcases to be generated in one kernel module at system level')
+    parser.add_argument('--dumpflag', type=bool, default=False,
+                        help='If instruction is `str`, dump the memory it writes to at system level')
     # Parse the command-line arguments
     args = parser.parse_args()
     instsfile = args.file
     mode = args.mode
     level = args.level
     outdir =f"{args.outdir}-{level}-{mode}"
-    return instsfile, mode, level, outdir
+    testnum = args.number
+    dumpflag = args.dumpflag
+    return instsfile, mode, level, outdir, testnum, dumpflag
 
 
 def main():
-    instsfile, mode, level, outdir = parse_args()
-
+    instsfile, mode, level, outdir, testnum, dumpflag = parse_args()
     insts = pickle.load(instsfile)
-
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
     if level == "user":
-        binary, offset = parse_template(mode, "user")
+        binary, offset, _ = parse_template(mode, "user")
         generate(binary, offset, insts, outdir)
     elif level == "system":
         binary, offset, offset_mem = parse_template(mode, "system")
-        kgenerate(binary, offset, insts, outdir)
+        kgenerate(binary, offset, offset_mem, insts, outdir, testnum, dumpflag)
 
 
 if __name__ == "__main__":
--- test-generator/utils/elf.py
+++ test-generator/utils/elf.py
@@ -23,24 +23,57 @@ def get_template_segments(mode):
     return get_segments(template_path)
 
 
-def parse_template(mode):
+def parse_template(mode, level):
     root_path = dirname(dirname(abspath(__file__)))
-    template_path = join(root_path, "test_template/template_{}".format(mode))
-    with open(template_path, "rb") as f:
-        elffile = ELFFile(f)
-        symtab = elffile.get_section_by_name(".symtab")
-        inst_location = symtab.get_symbol_by_name("inst_location")[0]
-        inst_location = inst_location.entry["st_value"]
-        for segment in elffile.iter_segments():
-            if segment.header["p_type"] == "PT_LOAD":
-                base_location = segment.header["p_vaddr"]
-                break
-        else:
-            raise ELFParseException("no load segment")
-        offset = inst_location - base_location
-        f.seek(0)
-        binary = f.read()
-    return binary, offset
+    if level == "user":
+        template_path = join(root_path, "test_template/user-level/template_{}".format(mode))
+        with open(template_path, "rb") as f:
+            elffile = ELFFile(f)
+            symtab = elffile.get_section_by_name(".symtab")
+            inst_location = symtab.get_symbol_by_name("inst_location")[0]
+            inst_location = inst_location.entry["st_value"]
+            for segment in elffile.iter_segments():
+                if segment.header["p_type"] == "PT_LOAD":
+                    base_location = segment.header["p_vaddr"]
+                    break
+            else:
+                raise ELFParseException("no load segment")
+            offset = inst_location - base_location
+            f.seek(0)
+            binary = f.read()
+        return binary, offset, None
+    
+    elif level == "system":
+        template_path = join(root_path, "test_template/system-level/ktemplate_{}.ko".format(mode))
+        with open(template_path, "rb") as f:
+            elffile = ELFFile(f)
+            symtab = elffile.get_section_by_name(".symtab")
+            inst_location = symtab.get_symbol_by_name("inst_location")[0]
+            inst_location = inst_location.entry["st_value"]
+            # dump_mem_location = symtab.get_symbol_by_name("dump_memory")[0]
+            # dump_mem_location = dump_mem_location.entry["st_value"]
+
+            text_section = elffile.get_section_by_name(".text")
+            text_section = text_section.header['sh_offset']
+            
+            # for segment in elffile.iter_segments():
+            #     if segment.header["p_type"] == "PT_LOAD":
+            #         base_location = segment.header["p_vaddr"]
+            #         break
+            # else:
+            #     raise ELFParseException("no load segment")
+            # offset = inst_location - base_location
+            offset = inst_location + text_section #if mode=='arm' else inst_location
+            # offset_mem = dump_mem_location + text_section #if mode=='arm' else dump_mem_location
+            offset_mem = None
+            f.seek(0)
+            binary = f.read()
+        return binary, offset, offset_mem
+        
+    else:
+        raise ELFParseException("unknown level")
+    
+    
 
 
 def get_locations(elfpath):
