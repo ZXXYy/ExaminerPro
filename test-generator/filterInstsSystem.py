3d572081b0425acb0ce0f7edd1ff94dd6e590a6e
xiaoyez <xiaoyez@zju.edu.cn>
2023-11-29 10:59:12 +0800

    add filter insts at system level

---
+++ test-generator/filterInstsSystem.py
@@ -0,0 +1,182 @@
+import click
+import logging
+import re
+import pickle
+import os
+from tqdm import tqdm
+from elftools.elf.elffile import ELFFile
+from capstone import (
+    Cs,
+    CsInsn,
+    CsError,
+    CS_ARCH_ARM,
+    CS_ARCH_ARM64,
+    CS_MODE_ARM,
+    CS_MODE_THUMB,
+)
+
+from keystone import (
+    Ks,
+    KsError,
+    KS_ARCH_ARM,
+    KS_ARCH_ARM64,
+    KS_MODE_ARM,
+    KS_MODE_LITTLE_ENDIAN,
+)
+from enum import Enum
+from utils import *
+class InstType(Enum):
+    STR = 1
+    BR = 2   
+    PREVILEGE = 3
+    OTHER = 4
+    INVALID = 5
+class StoreTestCaseGenerator:
+    def __init__(self, mode: str):
+        self.mode = mode
+        self.set_capstone()
+        self.set_keystone()
+
+    def set_capstone(self):
+        if self.mode == "arm":
+            cs = Cs(CS_ARCH_ARM, CS_MODE_ARM)
+        elif self.mode == "thumb":
+            cs = Cs(CS_ARCH_ARM, CS_MODE_THUMB)
+        elif self.mode == "arm64":
+            cs = Cs(CS_ARCH_ARM64, CS_MODE_ARM)
+        else:
+            raise ValueError("mode is undefined: {}".format(self.mode))
+        self.cs = cs
+
+    def set_keystone(self):
+        if self.mode in ("arm", "thumb"):
+            ks = Ks(KS_ARCH_ARM, KS_MODE_ARM)
+        elif self.mode == "arm64":
+            ks = Ks(KS_ARCH_ARM64, KS_MODE_LITTLE_ENDIAN)
+        else:
+            raise ValueError("mode is undefined: {}".format(self.mode))
+        self.ks = ks
+    
+    def get_inst_type(self, inst):
+        try:
+            cs_insn: CsInsn = next(self.cs.disasm(inst, 0x00), None)
+        except CsError as err:
+            logging.debug(err)
+        if cs_insn:
+            inst_name = cs_insn.insn_name()
+            op_str = cs_insn.op_str
+            # print(inst_name)
+            # print(op_str)
+            try:
+                if self.mode == 'arm64':
+                    if inst_name in A64_STR_INSTS:
+                        return InstType.STR
+                    if inst_name in A64_BR_INSTS:
+                        return InstType.BR
+                    dst = op_str.split(',')[0]
+                    if dst == 'pc' or dst == 'pc!' or dst == 'sp' or dst == 'wsp':
+                        # if dst =='wsp':
+                        #     print(inst_name)
+                        #     print(op_str)
+                        return InstType.BR
+                    if inst_name in A32_MUL_INSTS:
+                        dst = op_str.split(',')[1].strip()
+                        if dst == 'pc' or dst == 'pc!' or dst == 'sp' or dst == 'wsp':
+                            return InstType.BR
+                    if "[" in op_str:
+                        position = '[' + op_str.split('[')[1]
+                        base_addr = position.split(',')[0][1:]
+                        base_addr = base_addr[:-1] if "]" in base_addr else base_addr 
+                        if ("!" in position or "], " in position) and (base_addr=='pc' or base_addr=='sp'):
+                            return InstType.BR
+                    # ld1 {v29.s}[3], [sp], #4
+                    m = re.match(r"{.*}.*, \[(.*)\],.*", op_str)
+                    if m:
+                        if m.group(1) == 'sp':
+                            print(inst_name)
+                            print(op_str)
+                            return InstType.BR
+                    return InstType.OTHER
+                else:
+                    # cpsid, mcr, svc
+                    if inst_name in A32_MCR_INSTS:
+                        return InstType.PREVILEGE
+                    if inst_name in A32_INT_INSTS:
+                        return InstType.PREVILEGE
+                    if inst_name in ["sha256h2"]:
+                        # print(InstType.PREVILEGE)
+                        return InstType.PREVILEGE
+                    if inst_name in A32_STR_INSTS:
+                        position = '[' + op_str.split('[')[1]
+                        base_addr = position.split(',')[0][1:]
+                        base_addr = base_addr[:-1] if "]" in base_addr else base_addr 
+                        if ("!" in position or "], " in position) and (base_addr=='pc' or base_addr=='sp'):
+                            return InstType.BR
+                        return InstType.STR
+                    if inst_name in A32_BR_INSTS:
+                        return InstType.BR
+                    dst = op_str.split(',')[0]
+                    if dst == 'pc' or dst == 'pc!' or dst == 'sp':
+                        return InstType.BR
+                    if inst_name in A32_MUL_INSTS:
+                        dst = op_str.split(',')[1].strip()
+                        if dst == 'pc' or dst == 'pc!' or dst == 'sp':
+                            return InstType.BR
+                    if "[" in op_str:
+                        position = '[' + op_str.split('[')[1]
+                        base_addr = position.split(',')[0][1:]
+                        base_addr = base_addr[:-1] if "]" in base_addr else base_addr 
+                        if ("!" in position or "], " in position) and (base_addr=='pc' or base_addr=='sp'):
+                            return InstType.BR
+                    return InstType.OTHER
+            except KsError as err:
+                logging.debug("{}: {} {}; {}".format(
+                    err, cs_insn.mnemonic, op_str, inserted_inst))
+        return InstType.INVALID
+        
+
+@click.command()
+@click.argument("mode", type=click.Choice(["arm", "thumb", "arm64"]))
+@click.argument("instsfile", type=click.File("rb"))
+@click.option(
+    "--outdir",
+    default="testcases",
+    type=click.Path(resolve_path=True),
+)
+def main(mode, instsfile, outdir):
+    if mode=='arm':
+        output_suffix = "" 
+    elif mode=='arm64':
+        output_suffix = "64"
+    str_insts = {}
+    noraml_insts = {}
+    filter_insts = {}
+
+    insts = pickle.load(instsfile)
+    for id, (name, inst) in tqdm(insts.items()):
+        temp = inst
+        if "SHA" in name or "LDRD" in name:
+            filter_insts[len(filter_insts)] = (name, inst)
+        sg = StoreTestCaseGenerator(mode)
+        inst_type = sg.get_inst_type(inst)
+        # print(str(id)+" "+str(inst_type))
+        if inst_type == InstType.BR or inst_type == InstType.PREVILEGE:
+            filter_insts[len(filter_insts)] = (name, inst)
+        elif inst_type == InstType.STR:
+            str_insts[len(str_insts)] = (name, inst)
+        else:
+            noraml_insts[len(noraml_insts)] = (name, inst)
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    with open(f'{outdir}/pickled_str_insts{output_suffix}', 'wb') as f:
+        pickle.dump(str_insts, f)
+    with open(f'{outdir}/pickled_noraml_insts{output_suffix}', 'wb') as f:
+        pickle.dump(noraml_insts, f)
+    with open(f'{outdir}/pickled_filter_insts{output_suffix}', 'wb') as f:
+        pickle.dump(filter_insts, f)
+
+
+if __name__ == "__main__":
+    main()
--- test-generator/utils/__init__.py
+++ test-generator/utils/__init__.py
@@ -8,3 +8,145 @@ def configure_logging(log_level):
         raise ValueError(f"Invalid log level: {log_level}")
 
     logging.basicConfig(level=numeric_level, format='%(asctime)s - %(levelname)s - %(message)s')
+
+ERROR_NONE = 0b0
+ERROR_PHYSICAL = 0b1
+ERROR_EMULATOR = 0b10
+ERROR_BOTH = 0b11
+
+MODE = ("arm", "thumb", "arm64")
+
+MEM_SLOT_OFFSET_ARM32 = 44
+MEM_SLOT_OFFSET_ARM64 = 64
+
+A32_MCR_INSTS = (
+    "mcr",
+    "mcr2",
+    "mcrr",
+    "mcrr2"
+)
+A32_INT_INSTS = (
+    "cps",
+    "svc",
+    "hvc",
+    "hvceq",
+    "rfeia"
+)
+A32_MUL_INSTS = (
+    "umull",
+    "umlal",
+    "smull",
+    "smlal",
+    "umaal"
+)
+
+A32_STR_INSTS = (
+    "str",
+    "strh",
+    "strb",
+    "strd",
+    "strt",
+    "strht",
+    "strbt",
+    "strex",
+    "strexh",
+    "strexb",
+    "strexd",
+    "vstr"
+)
+A32_BR_INSTS = (
+    "b",
+    "cbnz",
+    "cbz",
+    "bl",
+    "blx",
+    "bx",
+    "bxj",
+    "tbb",
+    "tbh"
+)
+A64_BR_INSTS = (
+    "b",
+    "cbnz",
+    "cbz",
+    "tbz",
+    "tbnz",
+    "bl",
+    "blr",
+    "br",
+    "ret",
+    "blx",
+    "bx",
+    "bxj",
+    "tbb",
+    "tbh"
+)
+A64_STR_INSTS = (
+    "str",
+    "strb",
+    "strh",
+    "stur",
+    "sturb",
+    "sturh",
+    "sttr",
+    "sttrb",
+    "sttrh",
+    "stxr",
+    "stxrb",
+    "stxrh",
+    "stlr",
+    "stlrb",
+    "stlrh",
+    "stlur",
+    "stlurb",
+    "stlurh",
+    # "stlxr",
+    # "stlxrb",
+    # "stlxrh",
+    # "stp",
+    # "stnp",
+    # "stxp",
+    # "stlxp",
+    "stllrb",
+    "stllrh",
+    "stllr",
+    "st2",
+    "st3",
+    "st1",
+    "st4",
+    "stnp",
+    "stp",
+    "stur"
+)
+
+INST_ARM_NOP = b"\x00\xf0\x20\xe3"
+INST_ARM_BX_THUMB = b"\x01\x00\x8f\xe2\x10\xff\x2f\xe1"  # add r0, pc, #1; bx r0
+INST_THUMB16_NOP = b"\x00\xbf"  # nop
+INST_THUMB16_BX_ARM = b"\x78\x47"  # bx pc
+INST_THUMB32_MOV_R0_0 = b"\x4f\xf0\x00\x00"  # mov.w r0, #0
+INST_ARM_MOV_R1 = b"\x0f\x10\xa0\xe1"  # mov r1, pc  # previous code: mov, r1, #0xbd 
+INST_ARM_B_FINISH = b"\xfe\xff\xff\xea"
+INST_ARM_B_DUMP = b"\xfe\xff\xff\xea"
+INST_ARM64_MOV_R1 = b"\xa1\x17\x80\xd2"  # mov, x1, #0xbd
+
+INST_ARMv5_A32_NOP = b"\x00\x00\xa0\xe1"
+INST_ARMv6_A32_NOP = b"\x00\x00\xa0\xe1"
+INST_ARMv7_A32_NOP = b"\x00\xf0\x20\xe3"
+INST_ARMv7_T32_NOP = b"\x00\xbf"
+INST_ARMv8_A64_NOP = b"\x1F\x20\x03\xD5"
+
+INST_ARMv5_A32_MOV_R1_0xBD = b"\xbd\x10\xa0\xe3"
+INST_ARMv6_A32_MOV_R1_0xBD = b"\xbd\x10\xa0\xe3"
+INST_ARMv7_A32_MOV_R1_0xBD = b"\xbd\x10\xa0\xe3"
+INST_ARMv8_A64_MOV_X1_0xBD = b"\xa1\x17\x80\xd2"
+
+INST_ARMv7_A32_BX_THUMB = b"\x01\x00\x8f\xe2" + b"\x10\xff\x2f\xe1"  # add r0, pc, #1; bx r0;
+INST_ARMv7_T32_BX_ARM = b"\x78\x47"     # bx pc
+INST_ARMv7_T32_MOV_R0_R1 = b"\x08\x46"  # mov r0, r1
+
+# sub r3, pc, #40
+INST_ARMv5_A32_SAVE_PC = b"\x28\x30\x4f\xe2" + INST_ARMv5_A32_NOP
+INST_ARMv6_A32_SAVE_PC = b"\x28\x30\x4f\xe2" + INST_ARMv6_A32_NOP
+INST_ARMv7_A32_SAVE_PC = b"\x28\x30\x4f\xe2" + INST_ARMv7_A32_NOP
+# adr x3, .; sub x3, x3, #44
+INST_ARMv8_A64_SAVE_PC = b"\x03\x00\x00\x10\x63\xb0\x00\xd1"
